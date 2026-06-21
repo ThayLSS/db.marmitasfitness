c33d64b7963371c060c49ad0c8b3d187d02e9979
Thaynara Lima <thaynara.b3ar@gmail.com>
2026-06-21 13:32:35 -0300

    Remove dynamic port definition comment
    Remove commented code related to dynamic port definition.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Render define portas dinamicamente através do process.env.PORT
+
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
